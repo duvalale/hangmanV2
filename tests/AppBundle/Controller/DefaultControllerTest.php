0728f3804ae8a5e9578d9633aa85f18609a77eb5
Alexandra Duval <alex.10cops@hotmail.fr>
2017-03-02 12:29:01 +0100

    added functionnal test to try word form and adapted dictionnary of words

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -6,13 +6,43 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testIndex()
+    public function providerWords()
+    {
+        yield['anaconda', 'Yeah! You won this game!'];
+        yield['pizza', 'Oops, you lost this game!'];
+    }
+
+
+    /**
+     * @param $word
+     * @param $expectedH2
+     * @dataProvider providerWords
+     */
+    public function testIndex($word, $expectedH2)
     {
         $client = static::createClient();
 
         $crawler = $client->request('GET', '/');
 
+        //check we're on home page with 200 status code
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+
+        //Go on game page
+        $link = $crawler->selectLink('Game')->link();
+        $crawler = $client->click($link);
+        //check we reached the game page successfully
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+
+        //enter a word in try word form
+        $form = $crawler->selectButton('Let me guess...')->form();
+        $client->submit($form, ['word' => $word]);
+
+        $this->assertEquals(302, $client->getResponse()->getStatusCode());
+
+        $crawler = $client->followRedirect();
+
+        //get what is displayed as h2 after redirection
+        $h2 = $crawler->filter("#content h2")->text();
+        $this->assertSame($expectedH2, $h2);
     }
 }
